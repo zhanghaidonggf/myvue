0fd5b98f78545fad6fb44b708a7556e4f1f0cbbd
zhanghaidonggf <ai2010yu@163.com>
2017-07-05 18:44:59 +0800

    添加新路由和模块，并引入echarts

---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="header">
+    <div class="ui container">
+      <div class="four ui buttons">
+        <button class="ui black button"><router-link to="/index">自我简述</router-link></button>
+        <button class="ui black button"><router-link to="/road">我的成长之路</router-link></button>
+        <button class="ui black button"><router-link to="/index">爱好</router-link></button>
+        <button class="ui black button"><router-link to="/road">其他</router-link></button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+.header{
+    background-image: linear-gradient(to bottom,#222,#111);
+    background-repeat: repeat-x;
+    border-color: #252525;
+    background-color: #1b1b1b;
+    box-shadow: 0 1px 10px rgba(0,0,0,0.1);
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '../components/Hello'
 import Index from '../components/Index'
+import Road from '../components/Road'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/index',
       name: 'Index',
       component: Index
+    },
+    {
+      path:'/road',
+      name:'Road',
+      component:Road
     }
   ]
 })
